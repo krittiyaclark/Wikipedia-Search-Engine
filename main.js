6f3d6a516f5aaf27cd7fc6a12e79e459ca86c254
Krittiya Clark <krittiyaclark@gmail.com>
2017-07-03 23:08:17 -0400

    Search works but need to work with the bar deletes text when new search

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-// Using XMLHttpRequest
-$(function (){
-  // Using jQuery
-  $.ajax( {
-      url: "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json",
-      dataType: 'jsonp',
-      type: 'POST',
-      headers: { 'Api-User-Agent': 'Example/1.0' },
-      success: function(data) {
-         // do something with data
-         console.log('success', data);
-         
-      }
-  } );
-})
+$(document).ready(function() {
+  // Search button is clicked run
+  $("#search").click(function(){
+
+    // Search input .val();
+    var wikiSearch = $("#wiki-search").val();
+
+    // API url with search form
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ wikiSearch +"&format=json&callback=?";
+
+    // Wikipedia API ajax call
+          $.ajax({
+            type: "GET",
+            url: url,
+            async:false,
+            jsonp: "callback",
+            dataType: "json",
+          success: function(result){
+            // Get heading console.log(data[1][0]);
+       // Get description console.log(data[2][0]);
+       // Get link console.log(data[3][0]);
+            console.log(result);
+            $("#wiki-result").html('');
+            for(var i = 0; i < result[1].length; i++) {
+            $("#wiki-result").prepend("<li><a href= "+result[3][i]+">"+result[1][i] +"</a><p>"+result[2][i]+"</p></li>");
+            //$("#wiki-result").value = '';
+          }
+
+        }});
+    });
+});
